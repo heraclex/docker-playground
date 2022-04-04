1fd19de817e6a49dfda02d9cc846339eccb6e2bd
heraclex <toan.leebao@gmail.com>
2022-04-04 21:49:42 +0200

    adding trino

--- spark-hive-minio/minio/entrypoint.sh
+++ spark-hive-minio/minio/entrypoint.sh
@@ -15,17 +15,24 @@ while [ $(ps -aef | grep minio | grep 9001 | wc -l) != 1 ]; do  printf '.'; slee
 # https://docs.min.io/docs/minio-client-complete-guide.html
 mc alias set myminio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD --api S3v4
 
-# TODO: need to check if the bucket is exist before creating
-mc mb myminio/hive
-mc mb myminio/spark
-
 # https://docs.min.io/docs/minio-multi-user-quickstart-guide.html
 # Create new canned policy by name readwrite-policy using readwrite-policy.json policy file.
 # mc admin policy add myminio readwrite-policy $MINIO_HOME/readwrite-policy.json
-mc admin user add myminio hive hive12345
-mc admin user add myminio spark spark12345
 
-mc admin policy set myminio readwrite user=hive
-mc admin policy set myminio readwrite user=spark
+if [[ ! $(mc ls myminio | grep 'hive') ]] && [[ ! $(mc admin user list myminio | grep 'hive') ]]
+then
+    echo 'create bucket hive'
+    mc mb myminio/hive
+    mc admin user add myminio hive hive12345
+    mc admin policy set myminio readwrite user=hive
+fi
+
+if [[ ! $(mc ls myminio | grep 'spark') ]] && [[ ! $(mc admin user list myminio | grep 'spark') ]]
+then
+    echo 'create bucket spark'
+    mc mb myminio/spark
+    mc admin user add myminio spark spark12345
+    mc admin policy set myminio readwrite user=spark
+fi
 
 tail -f /dev/null
