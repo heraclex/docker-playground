7c7a4a6393073391691c36dc3527c60b5f99a6b5
heraclex <toan.leebao@gmail.com>
2022-04-05 13:21:31 +0200

    try out new hadoop version but does not work

--- spark-hive-minio/hive/entrypoint.sh
+++ spark-hive-minio/hive/entrypoint.sh
@@ -2,6 +2,9 @@
 
 echo "Replacing guava "
 rm -f $HIVE_HOME/lib/guava-*.jar && cp $HADOOP_HOME/share/hadoop/hdfs/lib/guava-*.jar $HIVE_HOME/lib/
+# rm -f $HIVE_HOME/lib/hadoop-aws-*.jar && cp $HADOOP_HOME/share/hadoop/hdfs/lib/hadoop-aws-*.jar $HIVE_HOME/lib/
 
 schematool -initSchema -dbType postgres
 start-metastore
+
+tail -f /dev/null
