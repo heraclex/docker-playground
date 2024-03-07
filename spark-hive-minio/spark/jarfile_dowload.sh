d5a6e03f3abad63f648d5cd5d5c70ed7825be3df
Toan Le <toan.lee@outlook.com>
2024-03-07 13:28:59 +0100

    testing spark

--- spark-hive-minio/spark/jarfile_dowload.sh
+++ spark-hive-minio/spark/jarfile_dowload.sh
@@ -1,27 +1,67 @@
 # cd airflow/dags/jars
 echo ${PWD}
 
-SPARK_VERSION=3.5.0
+SPARK_VERSION=3.5.1
 HADOOP_VERSION=3
 SPARK_PACKAGE="spark-${SPARK_VERSION}-bin-hadoop${HADOOP_VERSION}"
-SCALA_VERSION=2.12
 
+SCALA_VERSION=2.13
+spark_packages=(
+    "spark-hive_$SCALA_VERSION==$SPARK_VERSION"
+    "spark-hive-thriftserver_$SCALA_VERSION==$SPARK_VERSION"
+)
+
+# How to resolve Hadoop-aws dependencies in spark extra jars:
+# => https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-aws/3.3.4
+# => https://blog.devgenius.io/spark-streaming-write-to-minio-331f6c91d506
+
+hadoop_packages=(
+    'hadoop-aws==3.3.4'
+    'hadoop-common==3.3.4'
+    'hadoop-client==3.3.4'
+    'hadoop-mapreduce-client-core==3.3.4'
+)
+
+aws_packages=(
+    'aws-java-sdk==1.12.634'
+    'aws-java-sdk-bundle==1.12.634'
+    'aws-java-sdk-s3==1.12.634'
+)
+
+
+# download spark
 # curl  -L  "https://archive.apache.org/dist/spark/spark-${SPARK_VERSION}/${SPARK_PACKAGE}.tgz" \
 #         --output "${SPARK_PACKAGE}.tgz"
 
-# download spark jars
-curl  -L  "https://repo1.maven.org/maven2/org/apache/spark/spark-hive_${SCALA_VERSION}/${SPARK_VERSION}/spark-hive_${SCALA_VERSION}-${SPARK_VERSION}.jar" \
-        --output "jars/spark-hive_${SCALA_VERSION}-${SPARK_VERSION}.jar"
-curl  -L  "https://repo1.maven.org/maven2/org/apache/spark/spark-hive-thriftserver_${SCALA_VERSION}/${SPARK_VERSION}/spark-hive-thriftserver_${SCALA_VERSION}-${SPARK_VERSION}.jar" \
-        --output "jars/spark-hive-thriftserver_${SCALA_VERSION}-${SPARK_VERSION}.jar"
-
-curl  -L  "https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk/1.11.534/aws-java-sdk-1.11.534.jar" \
-        --output "jars/aws-java-sdk-1.11.534.jar"
-curl  -L  "https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.11.874/aws-java-sdk-bundle-1.11.874.jar" \
-        --output "jars/aws-java-sdk-bundle-1.11.874.jar"
+# download delta packages 
 curl  -L  "https://repo1.maven.org/maven2/io/delta/delta-core_2.12/2.4.0/delta-core_2.12-2.4.0.jar" \
         --output "jars/delta-core_2.12-2.4.0.jar"
-curl  -L  "https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.2.0/hadoop-aws-3.2.0.jar" \
-        --output "jars/hadoop-aws-3.2.0.jar"
+
+# download spark-packages jars
+for index in "${spark_packages[@]}" ; do
+    package_name="${index%%==*}"
+    package_version="${index##*==}"
+    echo "\n >>> donwload $package_name==$package_version...."
+    curl  -L  "https://repo1.maven.org/maven2/org/apache/spark/${package_name}/$package_version/${package_name}-${package_version}.jar" \
+        --output "jars/${package_name}-${package_version}.jar"
+done
+
+# download aws packages 
+for index in "${aws_packages[@]}" ; do
+    package_name="${index%%==*}"
+    package_version="${index##*==}"
+    echo "\n >>> donwload $package_name==$package_version...."
+    curl  -L  "https://repo1.maven.org/maven2/com/amazonaws/$package_name/$package_version/${package_name}-${package_version}.jar" \
+        --output "jars/${package_name}-${package_version}.jar"
+done
+
+# download hadoop packages 
+for index in "${hadoop_packages[@]}" ; do
+    package_name="${index%%==*}"
+    package_version="${index##*==}"
+    echo "\n >>> donwload $package_name==$package_version...."
+    curl  -L  "https://repo1.maven.org/maven2/org/apache/hadoop/$package_name/$package_version/${package_name}-${package_version}.jar" \
+        --output "jars/${package_name}-${package_version}.jar"
+done
 
 # cd ../../..
--- spark-hive-minio/spark/spark-master.sh
+++ spark-hive-minio/spark/spark-master.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "History server is sarting ...."
-
-start-history-server.sh
-
 echo "$(hostname -i) spark-master" >> /etc/hosts
 
 spark-class org.apache.spark.deploy.master.Master --ip 0.0.0.0 --port 7077 --webui-port 8080
+
+# echo "History server is sarting ...."
+# spark-class org.apache.spark.deploy.history.HistoryServer
